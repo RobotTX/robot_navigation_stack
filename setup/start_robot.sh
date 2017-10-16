da07f3e4582c9e385b42bd49af0a9c649bb8f3da
GTRoboTx <teng.xiao@gtrobots.com>
2017-10-16 10:09:00 +0800

    push to local server

--- setup/start_robot.sh
+++ setup/start_robot.sh
@@ -12,4 +12,5 @@ then
     gnome-terminal -x bash -c "source /opt/ros/kinetic/setup.bash;roscore;exec bash;"
     sleep 3s
 fi
-gnome-terminal -x bash -c "source /opt/ros/kinetic/setup.bash;source /home/gtdollar/catkin_ws/devel/setup.bash;roslaunch gobot_base mapbase.launch;exec bash;"
+gnome-terminal -x bash -c "source /opt/ros/kinetic/setup.bash;source /home/gtdollar/catkin_ws/devel/setup.bash;roslaunch gobot_software gobot_software.launch;exec bash;"
+gnome-terminal -x bash -c "source /opt/ros/kinetic/setup.bash;source /home/gtdollar/catkin_ws/devel/setup.bash;roslaunch gobot_navigation gobot_navigation.launch;exec bash;"
